7e69e3ae731cf2acf50c309989b48e88cfb72401
Papa Aggrey <aggreyian3@gmail.com>
2023-01-03 00:13:34 -0700

    task 3

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,20 +1,42 @@
 import { ServerRespond } from './DataStreamer';
 
-export interface Row {
-  stock: string,
-  top_ask_price: number,
+export interface Row { 
+  ratio: number,
+  priceofABC: number,
+  priceofDEF: number,
+  triggerPoint: number | undefined,
+  upperbound: number,
+  lowerbound: number,
   timestamp: Date,
 }
 
 
 export class DataManipulator {
-  static generateRow(serverResponds: ServerRespond[]) {
-    return serverResponds.map((el: any) => {
-      return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
-      };
-    })
+  static generateRow(serverRespond: ServerRespond[]): Row {
+    const priceABC = (serverRespond[0].top_ask.price + serverRespond[0].top_bid.price) / 2;
+    const priceDEF = (serverRespond[1].top_ask.price + serverRespond[1].top_bid.price) / 2;
+    const ratio = priceABC / priceDEF;
+    const upperbound = 1 + 0.05;
+    const lowerbound = 1 - 0.05;
+
+    return {
+      ratio,
+      priceofABC: priceABC,
+      priceofDEF : priceDEF,
+      upperbound,
+      lowerbound,
+      triggerPoint: (ratio > upperbound || ratio < lowerbound) ? ratio :undefined,
+      timestamp: serverRespond[0].timestamp > serverRespond[1].timestamp ? serverRespond[0].timestamp : serverRespond[1].timestamp,
+    }
+    
+  //   return serverResponds.map((el: any) => {
+  //     return {
+  //       stock: el.stock,
+  //       top_ask_price: el.top_ask && el.top_ask.price || 0,
+  //       timestamp: el.timestamp,
+  //     };
+  //   })
+  // }
+    
   }
 }
